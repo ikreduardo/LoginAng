a9e6c68dffb256080889e711b3e7b8b905c58861
ikreduardo <ikreduardo93@gmail.com>
2020-06-30 18:07:39 -0500

    My second commit Angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { HomeComponent } from './components/main/home/home.component';
+
+
+const routes: Routes = [
+    {path:'login', component: LoginComponent},
+    {path:'register', component: RegisterComponent},
+    {path:'', redirectTo: '/login', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: '**', redirectTo: '/login'}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { HomeComponent } from './components/main/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
